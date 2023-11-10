0921de9e476bcecbd30be127c43fafd0bacbba12
JonMS95 <jon_portugalete@hotmail.com>
2023-11-10 18:21:35 +0100

    Changed SocketInteract function prototype (server_socket).

--- Source_files/ClientSocketUse.h
+++ Source_files/ClientSocketUse.h
@@ -19,7 +19,7 @@
 int CreateSocketDescriptor(int domain, int type, int protocol);
 struct sockaddr_in PrepareForConnection(sa_family_t address_family, char* server_addr, uint16_t server_port);
 int SocketConnect(int socket_desc, struct sockaddr_in server);
-void SocketInteract(int new_socket, bool secure, SSL** ssl);
+void SocketInteract(int server_socket, bool secure, SSL** ssl);
 int CloseSocket(int new_socket);
 
 /*************************************/
