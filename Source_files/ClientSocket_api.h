254412fd0455b32fcdf638a0a41f61009e9fd16b
JonMS95 <jon_portugalete@hotmail.com>
2023-11-11 16:05:21 +0100

    Added Doxygen comment for ClientSocketRun.

--- Source_files/ClientSocket_api.h
+++ Source_files/ClientSocket_api.h
@@ -18,6 +18,12 @@ extern "C" {
 /******** Function prototypes ********/
 /*************************************/
 
+/// @brief Runs client socket.
+/// @param server_addr Target server address
+/// @param server_port Target server port.
+/// @param secure Enable secure communication (TLS).
+/// @param CustomSocketStateInteract Custom function to interact with server once connection is established.
+/// @return 0 always, exit sending failure signal if SIGINT signal handler could not be properly set.
 int ClientSocketRun(char* server_addr, int server_port, bool secure, void (*CustomSocketStateInteract)(int client_socket, bool secure, SSL** ssl));
 
 /*************************************/
